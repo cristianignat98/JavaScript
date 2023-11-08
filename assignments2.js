50a6574ee214d14a5893cdacb67bcaf3930f9b77
cristianignat98 <72981236+cristianignat98@users.noreply.github.com>
2023-11-08 13:57:03 +0200

    Add files via upload

--- assignments2.js
+++ assignments2.js
@@ -1,5 +1,5 @@
 ////////////Lecture Functions
-
+/*
 // function describeCountry(country, population, capitalCity) {
 //   return `${country} has ${population} million people and its capital city is ${capitalCity}`;
 // }
@@ -9,10 +9,10 @@
 // const describeItalia = describeCountry("Italia", 45, "Roma");
 
 // console.log(describeRomania, describeAnglia, describeItalia);
-
+*/
 ////////////Lecture Function Declarations vs Expressions
-//function declaration
 /*
+//function declaration
 function percentageofWorld1(population) {
   return (population / 7900) * 100;
 }
@@ -62,4 +62,113 @@ const describePopulation = function (country, population) {
 describePopulation("Portugal", 10);
 describePopulation("China", 1441);
 describePopulation("USA", 332);
+*/
+
+////////////Introduction to arrays
+/*
+const populations = [20, 56, 88, 107];
+console.log(populations.length === 4);
+
+function percentageofWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentages = [
+  percentageofWorld1(populations[0]),
+  percentageofWorld1(populations[1]),
+  percentageofWorld1(populations[2]),
+  percentageofWorld1(populations[3]),
+];
+console.log(percentages);
+*/
+
+//////////// Basic Array Operations (Methods)
+/*
+const neighbours = ["Bulgaria", "Moldova", "Ucraina", "Serbia", "Ungaria"];
+console.log(neighbours);
+
+const newNeighbour = neighbours.push("Utopia");
+console.log(neighbours);
+console.log(newNeighbour);
+
+const popped = neighbours.pop();
+console.log(neighbours);
+console.log(popped);
+
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not an Easten European country");
+}
+
+neighbours[neighbours.indexOf("Moldova")] = "Republica Moldova";
+console.log(neighbours);
+*/
+
+//////////// Challange 2
+/*
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+}; /// HERE WE USED TERNARY OPERATOR
+
+// U CAN DO WITH ARROW ALSO:
+// const calcTip = (bill) => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+console.log(bills, tips, totals);
+*/
+
+///////////// Iteration: The for Loop
+/*
+for (let i = 1; i < 51; i++) {
+  console.log(`Voter ${[i]} is currently voting`);
+}
+*/
+
+///////////// Looping Arrays, Breaking and Continuing
+/*
+const populations = [20, 56, 88, 107];
+function percentageofWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentages2 = [];
+
+for (let i = 0; i < populations.length; i++) {
+  const perc = percentageofWorld1(populations[i]);
+  percentages2.push(perc);
+}
+console.log(percentages2);
+*/
+
+///////////// Looping Backwards and Loops in Loops
+/*
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+  for (let y = 0; y < listOfNeighbours[i].length; y++) {
+    console.log(`Neighbour: ${listOfNeighbours[i][y]}`);
+  }
+}
+*/
+
+///////////// The while Loop
+/*
+const populations = [20, 56, 88, 107];
+function percentageofWorld1(population) {
+  return (population / 7900) * 100;
+}
+
+const percentages3 = [];
+let i = 0;
+while (i < populations.length) {
+  const perc = percentageofWorld1(populations[i]);
+  percentages3.push(perc);
+  i++;
+}
+console.log(percentages3);
 */
--- script.js
+++ script.js
@@ -35,9 +35,9 @@ console.log(appleJuice);
 */
 
 ///// FUNCTION DECLARATIONS VS. EXPRESSIONS
+/*
 
 // Function declarations :
-/*
 function calcAge1(birthYear) {
   return 2037 - birthYear;
 }
@@ -54,8 +54,8 @@ console.log(age1, age2);
 */
 
 //// ARROW FUNCTION
-// Arrow function is shorter and faster to write
 /*
+// Arrow function is shorter and faster to write
 const calcAge3 = (birthYear) => 2023 - birthYear; // this is also a value that we assign to the calcAge3 variable, just like the function expression, but this one is easier and faster to write. We don't need the curly braces {} and also the return happens implicity.
 
 const age3 = calcAge3(1999);
@@ -205,6 +205,7 @@ console.log(ages);
 */
 
 /////////// BASIC ARRAY OPERATIONS (METHODS)
+/*
 /// Add elements
 const friends = ["Michael", "Steven", "Peter"];
 friends.push("Jay"); /////// PUSH is a method, a function, and we call that function directly on the friends array AND IT ADDS AN ELEMENT AT THE END OF THE ARRAY
@@ -250,3 +251,376 @@ console.log(friends.includes(23));
 if (friends.includes("Peter")) {
   console.log("You have a friend called Peter");
 }
+*/
+
+//////////////////// INTRODUCTION TO OBJECTS
+/*
+//aici avem un array:
+const jonasArray = [
+  "Jonas",
+  "Cristian",
+  2023 - 1999,
+  "student",
+  ["Michael", "Peter", "Steven"],
+];
+
+//aici avem un object:
+const jonas = {
+  //THERE ARE MULTIPLE WAYS OF DOING OBJECTS, BUT WITH CURLY BRACES LIKE THIS IS THE EASIEST ONE AND ITS CALLED OBJECT LITERAL SYNTAX, bcs we are literally writing down the entire object content
+  firstName: "Jonas",
+  lastName: "Cristian",
+  age: 2023 - 1999,
+  job: "student",
+  friends: ["Michael", "Peter", "Steven"],
+}; /////// we have here an Object(which contains 5 properties- firstName,lastName,age,job,friends), each property has a VALUE
+*/
+
+////////////////// DOT VS. BRACKET NOTATION
+/*
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Cristian",
+  age: 2023 - 1999,
+  job: "student",
+  friends: ["Michael", "Peter", "Steven"],
+};
+console.log(jonas);
+
+/// THE 1st WAY OF GETTING A PROPERTY FROM AN OBJECT IS BY USING THE DOT NOTATION:
+console.log(jonas.lastName);
+/// THE 2nd WAY OF GETTING A PROPERTY FROM AN OBJECT IS BY USING THE BRACKETS NOTATION:
+console.log(jonas["job"]);
+
+const nameKey = "Name";
+console.log(jonas["first" + nameKey]);
+console.log(jonas["last" + nameKey]);
+
+const interestedIn = prompt(
+  "What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends"
+);
+
+if (jonas[interestedIn]) {
+  console.log(jonas[interestedIn]);
+} else {
+  console.log(
+    "Wrong request! Choose between firstName, lastName, age, job and friends"
+  );
+}
+
+////Using both the DOT and Bracket Notation to ADD NEW PROPERTIES to the OBJECT:
+jonas.location = "Romania"; /// that's how we use the DOT notation to add location property
+jonas["twitter"] = "@portugal"; /// here we used the brackets to add twitter property to the object
+console.log(jonas);
+
+//Challange
+// write 'Jonas has 3 friends, and his best friend is called Michael'
+console.log(
+  `${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`
+);
+*/
+////////////////// OBJECT METHODS
+/*
+/// Since functions are values, we can add them to an object
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Cristian",
+  birthYear: 1999,
+  job: "student",
+  friends: ["Michael", "Peter", "Steven"],
+  hasDriversLicence: true,
+
+  // calcAge: function (birthYear) {
+  //   return 2023 - birthYear;
+  // }, // aici calcAge is not a variable its just a property of the object
+
+  // const calcAge= function (birthYear) {
+  // return 2023 - birthYear;
+  // }  /// this is how we used to do it before
+
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2023 - this.birthYear; // jonas.birthYear would also work but we try do not repeat yourself principle, so when we would want to change the variable name to jonas2 for example, we would have to go through all jonas.smth to change it to jonas2.smth, and that's not ok
+  // },
+
+  calcAge: function () {
+    this.age = 2023 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is a ${this.calcAge()} year old ${
+      jonas.job
+    }, and he has ${this.hasDriversLicence ? "a" : "no"} driver's licence}`;
+  },
+};
+
+console.log(jonas.calcAge());
+
+console.log(jonas);
+console.log(jonas.age);
+console.log(jonas.age);
+
+//Challange
+// write "Jonas is a 24-year old student, and he has a/no drivers licence"
+
+console.log(jonas.getSummary());
+*/
+
+///////////// CHALLANGE 3
+// BMI = mass / (height * height)
+/*
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+
+// Log to the console 'John Smith's BMI() is higher than Mark Miller's ()'
+if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI(${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})`
+  );
+} else {
+  console.log(
+    `${mark.fullName}'s BMI(${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+  );
+}
+*/
+
+///////////// ITERATION: THE FOR LOOP
+/*
+// IF/ELSE statement it's a CONTROL structure, just like others, including LOOP and more
+// so let's say you go to the gym and you lift some weights and you are doing 1 exercise with 10 reps
+// this could be write like this:
+
+// console.log("Lifting weights repetition 1");
+// console.log("Lifting weights repetition 2");
+// console.log("Lifting weights repetition 3");
+// console.log("Lifting weights repetition 4");
+// console.log("Lifting weights repetition 5");
+// console.log("Lifting weights repetition 6");
+// console.log("Lifting weights repetition 7");
+// console.log("Lifting weights repetition 8");
+// console.log("Lifting weights repetition 9");
+// console.log("Lifting weights repetition 10");
+
+///instead of writing 10 times this code, we can create a loop and put one of these lines of code in there:
+
+// this condition will be evaluated before each iteration of the loop, if the condition it's TRUE, then the next loop iteration will run, when this condition is false, then the loop stops
+for (let rep = 1; rep <= 30; rep++) {
+  console.log(`Lifting weights repetition ${rep}`);
+}
+//this loop is keep running while the rep variable is less or equal 10
+// rep++ is basically rep= rep + 1, so for each line of code, loop calculates rep + 1 and execute it
+*/
+
+///////////////// LOOPING ARRAYS. BREAKING AND CONTINUING
+/*
+//////////////////////////////////// EXEMPLUL 1/////////////////////////////////////
+//aici avem un array:
+const jonas = [
+  "Jonas",
+  "Cristian",
+  2023 - 1999,
+  "student",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+// now we can use the for loop to loop through this array
+// let's say that we wanted to individually log every element of the array to the console:
+
+// console.log(jonas[0])
+// console.log(jonas[1])
+// ...
+// console.log(jonas[4])
+// jonas[5] does NOT exist
+
+for (let i = 0; i < jonas.length; i++) {
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+// type of jonas[i] arata langa fiecare element in consola ce anume este(string,boolean,object,number etc)
+// am pus i<jonas.length pentru a nu conditiona loop cu un anumit nr de variabile, iar in cazul in care vrem ulterior sa schimbam si sa adaugam inca un element sau mai multe in array, acestea sa poata fi redate de catre loop.
+// am pus i = 0 pentru ca un array incepe cu elementul 0
+//
+//
+//
+// now we create an array with the typeof elements from last array:
+const types = [];
+for (let i = 0; i < jonas.length; i++) {
+  // Reading from jonas array
+  console.log(jonas[i], typeof jonas[i]);
+
+  // Filling types array
+  //types[i] = typeof jonas[i]; // the 1st way
+  types.push(typeof jonas[i]);
+}
+console.log(types);
+
+
+//////////////////////////////////// EXEMPLUL 2 /////////////////////////////////////
+const years = [1991, 2007, 1969, 2020];
+// now we want to calculate the ages for all this four birth years here, and we want to store them in a new array:
+const ages = [];
+
+for (let i = 0; i < years.length; i++) {
+  ages.push(2023 - years[i]);
+}
+console.log(ages);
+
+/////////////////////////////// continue and break //////////////////////////////////
+// CONTINUE is to exit the current iteration of the loop and continue to the next one
+const jonas = [
+  "Jonas",
+  "Cristian",
+  2023 - 1999,
+  "student",
+  ["Michael", "Peter", "Steven"],
+  true,
+];
+// let's say we wanted to print elements from the array that are only strings, the continue statement it's perfect for this:
+console.log("--- ONLY STRINGS ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] !== "string") continue; // we say if the typeof the current element is NOT a string, then continue, which means that the current iteration of the loop is exited, and then the next one starts immediately
+  console.log(jonas[i], typeof jonas[i]);
+}
+
+//
+// BREAK is to completely terminate the whole loop
+// what we want now, is to log no other elements after we found a number
+// so basically after a number is found, NOTHING ELSE should be printed
+console.log("--- BREAK WITH NUMBER ---");
+for (let i = 0; i < jonas.length; i++) {
+  if (typeof jonas[i] === "number") break;
+  console.log(jonas[i], typeof jonas[i]); // nici macar acest cod nu mai este redat
+}
+//in array-ul jonas, the number ar fi 2023-1999 adica 24, deci break n a mai redat niciun element ce se afla dupa 2023-1999
+
+*/
+///////////////// LOOPING BACKWARDS AND LOOPS IN LOOPS
+/*
+
+//LOOPING BACKWARDS:
+const jonas = [
+  "Jonas",
+  "Cristian",
+  2023 - 1999,
+  "student",
+  ["Michael", "Peter", "Steven"],
+];
+// let's now loop over this array BACKWARDS(de la ultimul la primul): 4,3,2,..,0
+// am scris let i =jonas.length - 1 pentru a reda unul cate unul de la sfarsit la final
+for (let i = jonas.length - 1; i >= 0; i--) {
+  console.log(i, jonas[i]);
+}
+
+//LOOP INSIDE OF A LOOP:
+// back to the weight lifting exercise that we did in the previous lecture, where we had an exercise with 10 reps, but now let's say that we have three different exercises with 5 reps each
+
+for (let exercise = 1; exercise < 4; exercise++) {
+  console.log(`------- Starting exercise ${exercise}`);
+
+  for (let rep = 1; rep < 6; rep++) {
+    console.log(`Lifting weights repetition ${rep}`);
+  }
+}
+*/
+
+//////////////// THE WHILE LOOP
+/*
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep}`);
+// }
+
+//
+//
+
+let rep = 1;
+while (rep <= 10) {
+  //console.log(`WHILE:  Lifting weights repetition ${rep}`);
+  rep++;
+}
+
+// while does not really need a COUNTER(let rep=1), only the condition have to be there in order to stay true and to keep running
+// on this example we do not depend on a counter, but instead it will depend on a random variable:
+// we will basically roll a dice and then keep rolling the dice until we roll a six, then when we roll a six, we stop:
+
+//the roll of a dice is between 1 and 6
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+  if (dice === 6) console.log("Loop is about to end...");
+}
+*/
+
+////////////////// CHALLANGE 4
+/*
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+// Create an array called bills containing all 10 test bill values.
+// Create empty arrays for the tips and the totals (tips and totals)
+// Use the calcTip function we wrote before (included in the starter code) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+
+// TEST DATA: 22, 295, 176, 440, 37, 105, 10, 1100, 86, and 52.
+
+// BONUS:
+// Write a function calcAverage which takes an array called arr as an argument. This function calculates the average of all numbers in the given array. This is a DIFFICULT challenge (we haven't done this before)! Here is how to solve it if you feel like it:
+
+// First, you will need to add up all values in the array. To do the addition, start by creating a variable sum that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the sum variable. This way, by the end of the loop, you have all values added together.
+
+// To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+
+// Call the function with the totals array.
+
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
+}
+console.log(bills, tips, totals);
+
+// BONUS rezolvare:
+const calcAverage = function (arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum= sum + arr[i];
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage([2, 3, 7]));
+console.log(calcAverage(totals));
+console.log(calcAverage(tips));
+*/
